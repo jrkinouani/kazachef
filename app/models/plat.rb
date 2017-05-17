767767511867a01a3ff53f1ea7ee4b61981a5e92
jrkinouani <kinouani@gmail.com>
2017-05-17 15:36:02 +0400

    hotfix

--- app/models/plat.rb
+++ app/models/plat.rb
@@ -2,6 +2,6 @@ class Plat < ApplicationRecord
   acts_as_votable
   belongs_to :user
   has_many :comments
-  has_attached_file :image, styles: { medium: "300x300#", small: "100x100>" }
+  has_attached_file :image
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
